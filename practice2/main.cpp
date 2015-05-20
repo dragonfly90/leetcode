ff86fc54f834af8791969e3caeefa19678b42cdb
Liang Dong <ldong6@ncsu.edu>
2015-05-20 16:21:06 -0400

    Initial Commit

---
+++ practice2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice2
+//
+//  Created by Liang Dong on 5/20/15.
+//  Copyright (c) 2015 Liang Dong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
